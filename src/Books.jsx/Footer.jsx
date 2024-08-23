049ab774c93fb00730a02a68e3a600fb53796ab5
arif-aexa <142899347+arif-aexa@users.noreply.github.com>
2024-08-23 23:14:27 +0600

    Add files via upload

---
+++ src/Books.jsx/Footer.jsx
@@ -0,0 +1,15 @@
+
+export default function Footer() {
+  return (
+  <>
+      <footer className="py-6 md:py-8 bg-gray-400 rounded-xl pt-5 mt-5">
+      <div className="container mx-auto">
+        <p className="text-center  text-lg lg:text-base font-bold">
+          Copyright ©2024 | All rights reserved by Arif hasan
+        </p>
+      </div>
+    </footer>
+
+  </>
+  )
+}
